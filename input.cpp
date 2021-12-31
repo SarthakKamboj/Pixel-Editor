3cc20cbd4b2d68fa7f9e3cdaaa0a2244afe0d3fc
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-12-30 17:26:09 -0800

    separated input press logic from input down logic

--- input.cpp
+++ input.cpp
@@ -1,23 +1,69 @@
 #include "input.h"
 
+
+Input::Input() {
+	inputReleased.setAllTrue();
+}
+
 void Input::update() {
 
 	SDL_Event evnt;
 
 	SDL_PollEvent(&evnt);
 
+	inputDown = {};
+	inputPressed = {};
+
+	mousePressed = false;
+
 	switch (evnt.type) {
 	case SDL_QUIT:
 		quit = true;
 		break;
 	case SDL_MOUSEBUTTONDOWN:
+		mousePressed = true;
 		mouseDown = true;
 		break;
 	case SDL_MOUSEBUTTONUP:
 		mouseDown = false;
 		break;
-	case SDL_KEYDOWN:
+	case SDL_KEYDOWN: {
 		SDL_KeyCode keyCode = (SDL_KeyCode)evnt.key.keysym.sym;
+		switch (keyCode) {
+		case SDLK_w: {
+			inputDown.w = true;
+			if (inputReleased.w) {
+				inputPressed.w = true;
+				inputReleased.w = false;
+			}
+			break; }
+		case SDLK_f: {
+			inputDown.f = true;
+			if (inputReleased.f) {
+				inputPressed.f = true;
+				inputReleased.f = false;
+			}
+			break;
+		}
+		}
 		break;
 	}
+	case SDL_KEYUP: {
+		SDL_KeyCode keyCode = (SDL_KeyCode)evnt.key.keysym.sym;
+		switch (keyCode) {
+		case SDLK_w:
+			inputReleased.w = true;
+			break;
+		case SDLK_f:
+			inputReleased.f = true;
+			break;
+		}
+		break;
+	}
+	}
+
+	if (mouseDown) {
+		SDL_GetMouseState(&mouseState.x, &mouseState.y);
+	}
+
 }
--- input.h
+++ input.h
@@ -3,9 +3,33 @@
 #include "SDL.h"
 #include <iostream>
 
+struct MouseState {
+	int x, y;
+};
+
+struct InputInfo {
+	bool w, f;
+
+	void setAllTrue() {
+		w = true;
+		f = true;
+	}
+};
+
 class Input {
 public:
+	Input();
 	void update();
 	bool quit = false;
+
 	bool mouseDown = false;
+	bool mousePressed = false;
+
+	InputInfo inputDown;
+	InputInfo inputPressed;
+
+	MouseState mouseState;
+
+private:
+	InputInfo inputReleased;
 };
